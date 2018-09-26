a9a96f06e77458b292d3f98e8b0e5ab2b81ca310
Roland Liwag <rliwag@copperleaf.com>
2018-09-26 10:22:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Demo for Using Sinon and Unexpected Together
+
+Examples under `spec/` demonstrate some simple uses of `sinon` and `unexpected`
+working together using the `unexpected-sinon` plugin.
+
+## Links to Documentation
+ - [sinon](https://sinonjs.org)
+ - [unexpected](http://unexpected.js.org/)
---
+++ spec/widget-spec.js
@@ -0,0 +1,48 @@
+const unexpected = require('unexpected');
+const sinon = require('sinon');
+const unexpectedSinon = require('unexpected-sinon');
+const Widget = require('../lib/widget');
+
+describe('widget', () => {
+  const expect = unexpected.clone().installPlugin(unexpectedSinon);
+
+  const slowStub = sinon.stub();
+  const asyncStub = sinon.stub();
+  const complexStub = sinon.stub();
+
+  let widget;
+
+  beforeEach(() => {
+    slowStub.reset();
+    asyncStub.reset();
+    complexStub.reset();
+    widget = new Widget(slowStub, asyncStub, complexStub);
+  });
+
+  it('should do something slowly', () => {
+    slowStub.returns(0);
+
+    expect(widget.doSlow(100), 'to equal', 0);
+  });
+
+  it('should do something asynchronously', (done) => {
+    asyncStub.yields(null, 0);
+
+    widget.doAsync(100, (err, value) => {
+      if (err) {
+        return done.fail(err);
+      }
+
+      expect(value, 'to equal', 0);
+      done();
+    });
+  });
+
+  it('should do something complicated', () => {
+    complexStub.returns(0);
+
+    expect(widget.doComplex(100, 99, 98), 'to equal', 0);
+    expect(complexStub, 'was called with', 100, 99, 98);
+    expect(complexStub, 'was called times', 2);
+  });
+});
